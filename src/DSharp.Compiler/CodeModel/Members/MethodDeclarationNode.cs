a8b4cf7873038b5dee787ab149f062d69b8108fa
donluispanis <luisgonzalezaracil@hotmail.com>
2020-02-18 21:10:23 +0100

    Fixed and refactored overloaded name generation

--- src/DSharp.Compiler/CodeModel/Members/MethodDeclarationNode.cs
+++ src/DSharp.Compiler/CodeModel/Members/MethodDeclarationNode.cs
@@ -9,11 +9,14 @@ using DSharp.Compiler.CodeModel.Names;
 using DSharp.Compiler.CodeModel.Statements;
 using DSharp.Compiler.CodeModel.Tokens;
 using DSharp.Compiler.CodeModel.Types;
+using DSharp.Compiler.Helpers;
 
 namespace DSharp.Compiler.CodeModel.Members
 {
     internal class MethodDeclarationNode : MemberNode
     {
+        private const string UNDERSCORE = "_";
+        private const string DOLLAR = "$";
         private readonly NameNode interfaceType;
         private readonly AtomicNameNode name;
         private ParseNodeList constraints;
@@ -67,57 +70,11 @@ namespace DSharp.Compiler.CodeModel.Members
             {
                 if(isOverloadedMethod)
                 {
-                    string parameters = string.Join("_", Parameters.OfType<ParameterNode>().Select(
-                        n =>
-                        {
-                            if (n.Type is GenericNameNode genericNode)
-                            {
-                                string genericArguments = string.Join("$", genericNode.TypeArguments.Select(nn =>
-                                {
-                                    //if (nn is GenericNameNode)
-                                    //{
-                                    //    string args = "";
-                                    //    ParseNode node = nn;
-                                    //
-                                    //    while(nn is GenericNameNode genericNameNode)
-                                    //    {
-                                    //        args += genericNameNode.Name;
-                                    //    }
-                                    //}
-
-                                    if (nn is NameNode nameNode)
-                                    {
-                                        return nameNode.Name;
-                                    }
-
-                                    if(nn is TypeNode typeNode)
-                                    {
-                                        return Token.GetString(typeNode.Token.Type);
-                                    }
-
-                                    return nn.ToString();
-                                }));
-                                return GetTypeName(n) + "$" + genericArguments;
-                            }
-
-                            return GetTypeName(n);
-                        }));
-
-                    return name.Name + "_" + parameters;
+                    return ParameterParserHelper.GetSeparatedParameters(name.Name, Parameters, UNDERSCORE, DOLLAR); ;
                 }
 
                 return name.Name;
             }
-        }
-
-        private string GetTypeName(ParameterNode node)
-        {
-            if (node.Type.Token is IdentifierToken token)
-            {
-                return token.Identifier;
-            }
-
-            return Token.GetString(node.Type.Token.Type);
         }
 
         public ParseNodeList Parameters { get; }
---
+++ src/DSharp.Compiler/Helpers/ParameterParserHelper.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using DSharp.Compiler.CodeModel;
+using DSharp.Compiler.CodeModel.Members;
+using DSharp.Compiler.CodeModel.Names;
+using DSharp.Compiler.CodeModel.Tokens;
+using DSharp.Compiler.CodeModel.Types;
+
+namespace DSharp.Compiler.Helpers
+{
+    internal static class ParameterParserHelper
+    {
+        internal static string GetSeparatedParameters(string nodeName, ParseNodeList parameters, string paramSeparator, string genericParamSeparator)
+        {
+            string arguments = string.Join(paramSeparator, parameters.OfType<ParameterNode>().Select(
+                parameter =>
+                {
+                    if (parameter.Type is GenericNameNode genericNode)
+                    {
+                        return GenerateGenericName(parameter, genericNode, genericParamSeparator);
+                    }
+
+                    return GetTypeName(parameter);
+                }));
+
+            return nodeName + paramSeparator + arguments;
+        }
+
+        private static string GenerateGenericName(ParameterNode parameter, GenericNameNode genericNode, string separator)
+        {
+            string genericArguments = string.Concat(genericNode.TypeArguments.Select(node =>
+            {
+                string arguments = "";
+                GetArguments(node, separator, ref arguments);
+                return arguments;
+            }));
+            return GetTypeName(parameter) + separator + genericArguments;
+        }
+
+        private static string GetTypeName(ParameterNode node)
+        {
+            if (node.Type.Token is IdentifierToken token)
+            {
+                return token.Identifier;
+            }
+
+            return Token.GetString(node.Type.Token.Type);
+        }
+
+        private static void GetArguments(ParseNode parameter, string separator, ref string arguments)
+        {
+            if (parameter is TypeNode typeNode)
+            {
+                arguments += Token.GetString(typeNode.Token.Type) + separator;
+            }
+
+            if (!(parameter is GenericNameNode) && parameter is NameNode nameNode)
+            {
+                arguments += nameNode.Name + separator;
+            }
+
+            if (parameter is GenericNameNode genericNode)
+            {
+                arguments += genericNode.Name + separator;
+
+                foreach (ParseNode node in genericNode.TypeArguments)
+                {
+                    GetArguments(node, separator, ref arguments);
+                }
+            }
+        }
+    }
+}
